916fc2362e50d623782f7b083b8c1a72113bf6c9
Snahier <hansalmeida@hotmail.com>
2021-04-20 22:20:29 -0300

    Add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from "next/dist/next-server/lib/router/router"
+import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from "../styles/GlobalStyles"
+import { light } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={light}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,14 +1,48 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle, css } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
+  ${({ theme }) => css`
+    * {
+      box-sizing: border-box;
 
-    margin: 0;
-    padding: 0;
-  }
+      margin: 0;
+      padding: 0;
+    }
 
-  body {
-    background: #333333;
-  }
+    @media (max-width: 1080px) {
+      html {
+        font-size: 93.75%;
+      }
+    }
+
+    @media (max-width: 720px) {
+      html {
+        font-size: 87.5%;
+      }
+    }
+
+    body {
+      background: ${theme.gray50};
+    }
+
+    body,
+    input,
+    textarea,
+    button {
+      font: 500 1rem sans-serif;
+      color: ${theme.gray500};
+    }
+
+    h1 {
+      font-size: 2rem;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    button {
+      cursor: pointer;
+    }
+  `}
 `
