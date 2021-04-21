7f697e6bd9ea7a9dcaad8c1fcbb9c5e4d6a296e2
Snahier <hansalmeida@hotmail.com>
2021-04-20 22:54:50 -0300

    Move header to App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,31 @@
 import { AppProps } from "next/dist/next-server/lib/router/router"
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
+import { Header } from "../components/Header"
+import { Player } from "../components/Player"
 import { GlobalStyles } from "../styles/GlobalStyles"
 import { light } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={light}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <AppContainer>
+      <ThemeProvider theme={light}>
+        <GlobalStyles />
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </ThemeProvider>
+    </AppContainer>
   )
 }
+
+const AppContainer = styled.div`
+  display: flex;
+
+  main {
+    flex: 1;
+  }
+`
 
 export default MyApp
