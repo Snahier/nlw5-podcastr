09e27bd69b511b3ce8c4e7fd4ee9a7dde8fa9c7f
Snahier <hansalmeida@hotmail.com>
2021-04-21 22:58:44 -0300

    Format static data properties

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { format, parseISO } from "date-fns"
+import { ptBR } from "date-fns/locale"
 import { GetStaticProps } from "next"
 import { api } from "../services/api"
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString"
 
 type Episode = {
   id: string
   title: string
+  thumbnail: string
+  description: string
   members: string
+  duration: number
+  durationAsString: string
+  url: string
+  published_at: string
 }
 
 type HomeProps = {
@@ -15,7 +24,9 @@ export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+      <pre>
+        <p>{JSON.stringify(props.episodes, null, 2)}</p>
+      </pre>
     </div>
   )
 }
@@ -28,10 +39,29 @@ export const getStaticProps: GetStaticProps = async () => {
       _order: "desc",
     },
   })
+  console.log(data)
+
+  const episodes = data.map((episode: any) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      published_at: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }
