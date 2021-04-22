6edc532570d04a8d8d1153f80752271f6492fb02
Snahier <hansalmeida@hotmail.com>
2021-04-22 00:32:20 -0300

    Style episode page

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,7 +2,9 @@ import { format, parseISO } from "date-fns"
 import { ptBR } from "date-fns/locale"
 import { GetStaticPaths, GetStaticProps } from "next"
 import { Params } from "next/dist/next-server/server/router"
+import Image from "next/image"
 import { useRouter } from "next/router"
+import styled, { css } from "styled-components"
 import { api } from "../../services/api"
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString"
 
@@ -59,5 +61,143 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export default function Episode({ episode }: EpisodeProps) {
   const router = useRouter()
 
-  return <h1>{episode.title}</h1>
+  return (
+    <EpisodeContainer>
+      <ThumbnailContainer>
+        <GoBackButton>
+          <img src="/arrow-left.svg" alt="Voltar" />
+        </GoBackButton>
+
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+
+        <PlayButton>
+          <img src="/play.svg" alt="Tocar" />
+        </PlayButton>
+      </ThumbnailContainer>
+
+      <Header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.published_at}</span>
+        <span>{episode.durationAsString}</span>
+      </Header>
+
+      <Description dangerouslySetInnerHTML={{ __html: episode.description }} />
+    </EpisodeContainer>
+  )
 }
+
+const EpisodeContainer = styled.section`
+  margin: 0 auto;
+  max-width: 45rem;
+  padding: 3rem 2rem;
+`
+
+const ThumbnailContainer = styled.div`
+  position: relative;
+
+  img {
+    border-radius: 1rem;
+  }
+`
+
+const buttonStyles = css`
+  position: absolute;
+  z-index: 5;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 3rem;
+  height: 3rem;
+
+  border: 0;
+  border-radius: 0.75rem;
+
+  font-size: 0;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`
+
+const GoBackButton = styled.button`
+  ${buttonStyles}
+  ${({ theme }) => css`
+    left: 0;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    background: ${theme.purple500};
+  `}
+`
+
+const PlayButton = styled.button`
+  ${buttonStyles}
+  ${({ theme }) => css`
+    right: 0;
+    top: 50%;
+    transform: translate(50%, -50%);
+
+    background: ${theme.green500};
+  `}
+`
+
+const Header = styled.header`
+  ${({ theme }) => css`
+    padding-bottom: 1rem;
+
+    border-bottom: 1px solid ${theme.gray100};
+
+    h1 {
+      margin-top: 2rem;
+      margin-bottom: 1.5rem;
+    }
+
+    span {
+      display: inline-block;
+
+      font-size: 0.875rem;
+
+      & + span {
+        position: relative;
+
+        margin-left: 1rem;
+        padding-left: 1rem;
+
+        &::before {
+          content: "";
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translate(-50%, -50%);
+
+          width: 4px;
+          height: 4px;
+
+          border-radius: 2px;
+          background: #ddd;
+        }
+      }
+    }
+  `}
+`
+
+const Description = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.gray800};
+    margin-top: 2rem;
+
+    line-height: 1.675rem;
+
+    p {
+      margin: 1.5rem 0;
+    }
+  `}
+`
