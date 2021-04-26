99334b04de9818211aadce09be8dc436a1d3baff
Snahier <hansalmeida@hotmail.com>
2021-04-25 21:31:46 -0300

    Export Context logic from App

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 type Episode = {
   title: string
@@ -18,3 +18,43 @@ type PlayerContextData = {
 }
 
 export const PlayerContext = createContext({} as PlayerContextData)
+
+type PlayerContextProviderProps = {
+  children: React.ReactNode
+}
+
+export const PlayerContextProvider = ({
+  children,
+}: PlayerContextProviderProps) => {
+  const [episodeList, setEpisodeList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const play = (episode: Episode) => {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying)
+  }
+
+  const setPlayingState = (state: boolean) => {
+    setIsPlaying(state)
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        isPlaying,
+        play,
+        togglePlay,
+        setPlayingState,
+      }}>
+      {children}
+    </PlayerContext.Provider>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,50 +1,23 @@
 import { AppProps } from "next/dist/next-server/lib/router/router"
-import { useState } from "react"
 import styled, { ThemeProvider } from "styled-components"
 import { Header } from "../components/Header"
 import { Player } from "../components/Player"
-import { PlayerContext } from "../contexts/PlayerContext"
+import { PlayerContextProvider } from "../contexts/PlayerContext"
 import { GlobalStyles } from "../styles/GlobalStyles"
 import { light } from "../styles/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [episodeList, setEpisodeList] = useState([])
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
-  const [isPlaying, setIsPlaying] = useState(false)
-
-  const play = (episode) => {
-    setEpisodeList([episode])
-    setCurrentEpisodeIndex(0)
-    setIsPlaying(true)
-  }
-
-  const togglePlay = () => {
-    setIsPlaying(!isPlaying)
-  }
-
-  const setPlayingState = (state: boolean) => {
-    setIsPlaying(state)
-  }
-
   return (
     <AppContainer>
       <ThemeProvider theme={light}>
         <GlobalStyles />
-        <PlayerContext.Provider
-          value={{
-            episodeList,
-            currentEpisodeIndex,
-            isPlaying,
-            play,
-            togglePlay,
-            setPlayingState,
-          }}>
+        <PlayerContextProvider>
           <main>
             <Header />
             <Component {...pageProps} />
           </main>
           <Player />
-        </PlayerContext.Provider>
+        </PlayerContextProvider>
       </ThemeProvider>
     </AppContainer>
   )
